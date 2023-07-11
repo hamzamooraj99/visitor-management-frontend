099b8dbe64429ef0e665c777edb4ec78d7585d95
hashir4560 <haashirraja@gmail.com>
2023-07-12 01:40:59 +0500

    Login Done

---
+++ src/components/Login.js
@@ -0,0 +1,40 @@
+// src/components/Login.js
+import React, { useState } from "react";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Logging in...");
+  };
+
+  return (
+    <div className="login-container">
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
+      <p>Enter Credentials To login</p>
+    </div>
+  );
+};
+
+export default Login;
